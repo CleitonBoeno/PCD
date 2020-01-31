ebfa662d5de37c74a980edb0fdf0ff0ecc74f383
Visitante7 <cboeno1822@gmail.com>
2020-01-31 11:15:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/app_pcd/CadastroPCD.java
@@ -0,0 +1,14 @@
+package com.example.app_pcd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroPCD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_pcd);
+    }
+}
---
+++ app/src/main/java/com/example/app_pcd/QuemSouEu.java
@@ -0,0 +1,14 @@
+package com.example.app_pcd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuemSouEu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quem_sou_eu);
+    }
+}
